b64077931902aa9345ae2c69cd4c3c3d4963f8c6
1PrishaJain <prishajain2607@gmail.com>
2026-07-20 14:13:14 -0700

    Create main.py

---
+++ Critic Agent/main.py
@@ -0,0 +1,134 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+import time
+
+# Critic Agent LLM
+# (temporary: Ollama)
+# Later replace with ChatOpenAI
+
+
+llm = ChatOllama(
+    model="llama3.1",
+    temperature=0
+)
+
+
+# Input from user (right now harcoded)
+user_question = """
+Why is my REAL ESSI simulation failing?
+"""
+
+
+# Temporary Agent #2 answer
+agent2_answer = """
+The simulation is failing becuase the sky is purple
+"""
+
+
+critic_prompt = ChatPromptTemplate.from_template(
+"""
+You are a critic agent for an engineering AI system.
+Evaluate the answer using your engineering knowledge.
+User Question:
+{question}
+
+Agent #2 Answer:
+{answer}
+
+Score the answer from 0–100.
+
+Approval Rule:
+- If the accuracy score is 90% or higher, return Status: APPROVED.
+- If the accuracy score is below 90%, return Status: FLAGGED.
+
+Return ONLY this format:
+
+Accuracy Score: __%
+
+Status: APPROVED or FLAGGED
+
+
+Overall:
+<2 short sentence>
+
+Do not write anything else.
+"""
+)
+
+
+
+# Create final prompt
+final_prompt = critic_prompt.format(
+    question=user_question,
+    answer=agent2_answer
+)
+
+
+
+
+# Run Critic Agent
+response = llm.invoke(final_prompt)
+
+
+#measure run time
+start_time = time.perf_counter()
+response = llm.invoke(final_prompt)
+end_time = time.perf_counter()
+
+print(response.content)
+
+# Runtime
+print("\n----- Performance -----")
+print(f"Runtime: {end_time - start_time:.3f} seconds")
+
+
+# Token count
+try:
+    usage = response.response_metadata["token_usage"]
+    input_tokens = usage["prompt_tokens"]
+    output_tokens = usage["completion_tokens"]
+    total_tokens = usage["total_tokens"]
+
+except Exception:
+    input_tokens = None
+    output_tokens = None
+    total_tokens = None
+
+
+# Cost Calculation
+
+if total_tokens is not None:
+    input_cost = (input_tokens / 1_000_000) * 0.40
+    output_cost = (output_tokens / 1_000_000) * 1.60
+    total_cost = input_cost + output_cost
+
+else:
+    input_cost = None
+    output_cost = None
+    total_cost = None
+
+
+
+# CO2 Estimate
+if total_tokens is not None:
+    ENERGY_PER_TOKEN = 0.000000086  # kWh/token
+    energy_kwh = total_tokens * ENERGY_PER_TOKEN
+    CO2_PER_KWH = 0.445
+    estimated_co2 = energy_kwh * CO2_PER_KWH
+
+else:
+    energy_kwh = None
+    estimated_co2 = None
+
+
+# Print
+print(response.content)
+print("\n------ Statistics ------")
+print(f"Runtime: {end_time-start_time:.2f} seconds")
+print(f"Input Tokens: {input_tokens}")
+print(f"Output Tokens: {output_tokens}")
+print(f"Total Tokens: {total_tokens}")
+print(f"Input Cost: {input_cost}")
+print(f"Output Cost: {output_cost}")
+print(f"Total Cost: {total_cost}")
+print(f"Estimated CO2: {estimated_co2}")
